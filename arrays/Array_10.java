c890e882502527207476907862e4ce5dbde99c40
Abhinava-svg <shrivastavaabhinava52@gmail.com>
2026-04-07 00:18:54 +0530

    Kadanes algorithm

---
+++ arrays/Array_10.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Array_10 {
+    public static void Kadanes(int numbers[]){
+        int currsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for(int i = 0; i<numbers.length; i++){
+            currsum = currsum + numbers[i];
+            if(currsum<0){
+                currsum = 0;
+            }
+            maxsum = Math.max(currsum, maxsum);
+        }
+        System.out.println("our max subarray is: " + maxsum);
+    }
+    public static void main(String[] args){
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        Kadanes(numbers);
+        
+    }
+}
