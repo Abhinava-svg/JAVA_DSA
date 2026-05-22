6d6bf636e8c8fad013565c186740f715bf42ee66
Abhinava-svg <shrivastavaabhinava52@gmail.com>
2026-05-22 19:36:49 +0530

    initial commit

---
+++ sorting/sorting_02.java
@@ -0,0 +1,31 @@
+public class sorting_02 {
+    public static void Selectionsort(int arr[]){
+        for(int i = 0; i < arr.length-1; i++){
+            int minpos = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[minpos] < arr[j]){
+
+                    minpos = j;
+                }
+
+                // swap
+                int temp = arr[minpos];
+                arr[minpos] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+     
+    public static void main(String[] args){
+        int arr[] = {5, 4, 1, 3, 2};
+        Selectionsort(arr);
+        printArr(arr);
+    }
+}
